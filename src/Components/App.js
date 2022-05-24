901f82f9443d20323cc4107df0372ae40293b4f4
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-05-24 13:40:11 -0600

    Add font and additional layout adjustments.

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,18 +10,18 @@ const App = () => {
 
 return (
   <section className='app'>
-    <h1 className='rollies-title'>Rollies!!</h1>
+    <h1 className='rollies-title title'>Rollies!!</h1>
     <div className='all-sections-div'>
       <section className='player-one-section'>
-        <h1>Roller One</h1>
+        <h1 className='player-one-title title'>Roller One</h1>
         
       </section>
       <section className='center-section'>
         <img className='blue-dice-png-button' src={dice} alt='red 20 sided polyhedral dice'/>
-        <h2>Roll The Dice!!</h2>
+        <h2 className='title'>Roll The Dice!!</h2>
       </section>
       <section className='player-two-section'>
-        <h1>Roller Two</h1>
+        <h1 className='player-two-title title'>Roller Two</h1>
 
       </section>
     </div>
