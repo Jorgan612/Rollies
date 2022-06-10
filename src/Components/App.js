15298569df6824075f3b9680c53177a156983e16
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-06-10 12:23:02 -0600

    Add state.

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,11 +4,18 @@ import dice from '../assets/d20-blue.png';
 
 const App = () => {
  const d20 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
- 
-  // state for each player 
-  // state for whos turn 
+
+ const [currentRoll, setCurrentRoll] = useState(0)
+ const [isPlayerOneTurn, setIsPlayerOneTurn] = useState(true);
+  // state for each player (player 1, player 2)
+  // state for whos turn it currently is
   // 
   
+
+  const getDiceRoll = () => {
+    let newRoll = Math.floor(Math.random() * d20.length);
+    setCurrentRoll(newRoll)
+  };
 
 return (
   <section className='app'>
@@ -19,7 +26,7 @@ return (
         
       </section>
       <section className='center-section'>
-        <img className='blue-dice-png-button' src={dice} alt='red 20 sided polyhedral dice'/>
+        <img className='blue-dice-png-button' src={dice} alt='red 20 sided polyhedral dice' onClick={getDiceRoll}/>
         <h2 className='title'>Roll The Dice!!</h2>
       </section>
       <section className='player-two-section'>
