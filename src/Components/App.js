9c01d2d48e9af06256ab500fde66c00e1a74ece1
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-06-17 14:40:40 -0600

    Continue to trouble shoot issues in event order/ conditions being met.

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,13 +7,12 @@ const App = () => {
  const [playerTwoRoll, setPlayerTwoRoll] = useState(null);
  const [isPlayerOneTurn, setIsPlayerOneTurn] = useState(true);
  const [winningPlayer, setWinningPlayer] = useState('');
- const [playerOneWins, setPlayerOneWins] = useState(0);
- const [playerTwoWins, setPlayerTwoWins] = useState(0);
+ const [playerOneWins, setPlayerOneWins] = useState(null);
+ const [playerTwoWins, setPlayerTwoWins] = useState(null);
   
 
   const getDiceRoll = () => {
     let newRoll = Math.floor(Math.random() * 20 + 1);
-    console.log('newroll', newRoll)
     if (isPlayerOneTurn) {
       setPlayerOneRoll(newRoll);
       setIsPlayerOneTurn(false);
@@ -27,50 +26,34 @@ const App = () => {
 
   const determineWinner = () => {
     // better BUT now the winningPlayer is off / inaccurate
-    // will only sometimes say player two wins!
     // draw condition written BUT will also show draw if you click too quickly and one of the values is still Null. 
     // setTimeout is not preventing additional clicks
     // and if clicked during set timeout the new roll for the next player is cleared at the end of the setTimeout.
 
-
-    // Draw does note work 
-    // conditions aren't working for player 2 and draw
-
-
     if (playerOneRoll > playerTwoRoll) {
       setWinningPlayer('Roller One Wins!');
       addPlayerWin();
-      console.log('player 1 won')
-    } 
+    }; 
     if (playerOneRoll < playerTwoRoll) {
       setWinningPlayer('Roller Two Wins!');
       addPlayerWin();
-      console.log('player 2 wins')
-    } 
+    }; 
     if (playerOneRoll === playerTwoRoll) {
       setWinningPlayer('DRAW!');
-      console.log('DRAW!')
-    }
-    // setTimeout(() => {
-    //   setPlayerOneRoll(null);
-    //   setPlayerTwoRoll(null);
-    //   setWinningPlayer('');
-    // }, "2000");
+    };
   }
 
   const addPlayerWin = () => {
     //function is invoked, but conditions are not met.
-    // object in state might be tricky using hooks per google (investigate further)
-    // console.log('add player win function')
     if (winningPlayer === 'Roller One Wins!') {
-      // playerWins.playerOneWins++;
       setPlayerOneWins(playerOneWins + 1);
-      console.log('addPlayerWin condition work?')
+      console.log('setplayerONEwins', playerOneWins)
     } 
     if (winningPlayer === 'Roller Two Wins!') {
       setPlayerTwoWins(playerTwoWins + 1);
+      console.log('setplayerTWOwins', playerTwoWins)
     }
-    // if (winningPlayer !== 'Roller One Wins!' || winningPlayer !== 'Roller Two Wins!') {
+    // if (winningPlayer === 'DRAW!') {
     //   return;
     // }
 
@@ -87,7 +70,7 @@ return (
     <div className='all-sections-div'>
       <section className='player-one-section'>
         <h1 className='player-one-title title'>Player One</h1>
-        {!playerOneWins && <p>Total Wins: {playerOneWins}</p>}
+        {playerOneWins && <p>Total Wins: {playerOneWins}</p>}
       </section>
       <section className='center-section'>
         <img className='blue-dice-png-button' src={dice} alt='Blue 20 sided polyhedral dice' onClick={getDiceRoll}/>
@@ -98,7 +81,7 @@ return (
       </section>
       <section className='player-two-section'>
         <h1 className='player-two-title title'>Player Two</h1>
-        {!playerTwoWins && <p>Total Wins: {playerTwoWins}</p>}
+        {playerTwoWins && <p>Total Wins: {playerTwoWins}</p>}
       </section>
     </div>
   </section>
